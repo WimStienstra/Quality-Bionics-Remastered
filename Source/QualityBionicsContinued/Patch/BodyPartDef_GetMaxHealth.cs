b3724c2e57d772832a76dd99b74021ba0db2b207
Vincent Wong <vincentwwy168@gmail.com>
2024-05-19 17:55:25 +0800

    Optimize GetMaxHealth patch

--- Source/QualityBionicsContinued/Patch/BodyPartDef_GetMaxHealth.cs
+++ Source/QualityBionicsContinued/Patch/BodyPartDef_GetMaxHealth.cs
@@ -9,29 +9,17 @@ namespace QualityBionicsContinued.Patch;
 [HarmonyPatch(typeof(BodyPartDef), nameof(BodyPartDef.GetMaxHealth))]
 public class BodyPartDef_GetMaxHealth
 {
-    private static bool? _skipCached = null;
-    private static bool Skip
+    [HarmonyPrepare]
+    private static bool ShouldPatch()
     {
-        get
-        {
-            if (!_skipCached.HasValue)
-            {
-                // Skip if EBF is running
-                _skipCached = LoadedModManager.RunningMods.Any(m => m.PackageIdPlayerFacing == "V1024.EBFramework");
-            }
-            return _skipCached.Value;
-        }
+        // Skip if EBF is running
+        QualityBionicsMod.WarningOnce("Skipping BodyPartDef_GetMaxHealth patch since EBF is present", 0x1337 + 0x69 - 0x420 + 0x1986);
+        return !LoadedModManager.RunningMods.Any(m => m.PackageIdPlayerFacing == "V1024.EBFramework");
     }
 
     [HarmonyPriority(Priority.Last)]
     private static void Postfix(BodyPartDef __instance, Pawn pawn, ref float __result)
     {
-        if (Skip)
-        {
-            QualityBionicsMod.WarningOnce("Skipping BodyPartDef_GetMaxHealth patch since EBF is present", 0x1337 + 0x69 - 0x420 + 0x1986);
-            return;
-        }
-
         foreach (var hediff in pawn.health.hediffSet.hediffs)
         {
             if (hediff.Part?.def == __instance)
